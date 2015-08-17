5f318d14c8d9a48bd3d0eacd36aca5ae08087945
raja csp <raja.csp@gmail.com>
2015-08-17 12:35:41 -0400

    AODA-02: read method provided

--- aoda/src/ca/aoda/ReadWeb.java
+++ aoda/src/ca/aoda/ReadWeb.java
@@ -1,8 +1,8 @@
 package ca.aoda;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -10,44 +10,52 @@ import org.jsoup.nodes.Element;
 
 public class ReadWeb {
 	
-	public static void main(String[] args){
-		String title = "dog";
-        String url =    "https://www.playolg.ca/content/olg/en/getting-started/whyplayolg.html";
-        try {
+	private static final String STARTING_PAGE = "https://www.playolg.ca/content/olg/en.html";
+	
+	public static void main(String[] args){		
+        String url =    STARTING_PAGE;
+        Set<String> set = new LinkedHashSet<String>();
+        
+        set = getPageLinks(url, set);
+        
+        for(String link:set){
+        	System.out.println(link);
+        }
+        
+        System.out.println(set.size());
+	}
+	
+	private static Set<String> getPageLinks(String url, Set<String> set){
+		try {
             Document doc = Jsoup.connect(url).get();
             
-            /*
-            Elements img = doc.getElementsByTag("a");
-
-            for (Element el : img) {
-                String src1 = el.absUrl("imgurl");
-                String src2 = el.absUrl("surl");
-                System.out.println(src1 + " " + src2);      
-            }
-            */            
-            //System.out.println(doc);
-            
-            List<String> links = new ArrayList<>();
-            List<String> filteredlinks = new ArrayList<>();
-            
             for (Element link : doc.select("a[href]")) {
-                //System.out.println(link.attr("href"));
             	
-            	String currentLink = link.attr("href"); 
-            	
-            	links.add(currentLink);            	
+            	String currentLink = link.attr("href");            	
+            	            	
             	if(currentLink.startsWith("/content/") && currentLink.endsWith(".html")){
-            		filteredlinks.add("https://www.playolg.ca"+currentLink);
+            		//filteredlinks.add("https://www.playolg.ca"+currentLink);
+            		
+            		String currentLinkFull = "https://www.playolg.ca"+currentLink;
+            		set.add(currentLinkFull);
+            		
+            		if(!set.contains(currentLinkFull)){
+            			//System.out.println(currentLinkFull);            		
+            			getPageLinks(currentLinkFull, set);
+            		}
             	}
             }
-            
-            
-            for(String link:filteredlinks){
-            	System.out.println(link);
-            }
-            
         } catch (IOException e) {
-            e.printStackTrace();
+            //e.printStackTrace();
+        	System.err.println("Error : "+e.getMessage());
         }
+		
+		return set;
 	}
+	
+	// find img tab without alt
+	
+	// W616 The 'language' attribute is obsolete:
+	
+	// 
 }
